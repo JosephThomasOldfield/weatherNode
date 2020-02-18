1a947bb014e2696f5bc54500cd0227d62150b3c9
joseph oldfield <joseph.t.oldfield@gmail.com>
2020-02-18 14:32:03 +0000

    secondcommit

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
+// -- // USING STANDARD METHOD (NO PLUGINS) // -- //
+
+// const request = require("request");
+// const {promisify} = require('util') 
+
+// const getWeather = () => {
+//     request({
+//         uri: `https://api.openweathermap.org/data/2.5/find?q=London,uk&APPID=${process.env.APPID}`,
+//         json: true
+//     }, (err, res) => {
+//         if(err) throw err;
+//         console.log(res.body)
+//     });
+// }
+
+// getWeather()
+
+
+// --- // USING PROMISIFY // --- // 
+
 const request = require("request");
-const APPID = '6b8bd397e514533bbb173e6a03429c98'
+const {promisify} = require('util'); 
+const fs = require('fs');
 
-const getWeather = () => {
-    request({
-        uri: `https://api.openweathermap.org/data/2.5/find?q=London,uk&APPID=${APPID}`,
-        json: true
-    }, (err, res) => {
-        if(err) throw err;
-        console.log(res.body)
+require('dotenv').config()
+
+const promisifiedRequest = promisify(request);
+
+const getWeather = async () => {
+    let data = await promisifiedRequest({
+        uri: `https://api.openweathermap.org/data/2.5/find?q=London,uk&APPID=${process.env.APPID}`,
+        json: false
+        
     });
-}
+    fs.writeFileSync("weatherData.json", data.body)
+};
 
-getWeather()
+getWeather();
+
+// Await - waits for the requests to complete before moving to the next line of code
+// This is astnchronos. This copuld also be done manually by replacing await with a function that returns the data,
+// however this is over complex for little gain.
+
